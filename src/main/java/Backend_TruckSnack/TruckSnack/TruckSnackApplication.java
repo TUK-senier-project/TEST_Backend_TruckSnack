79614523e5f314c6f35c00078e80ba3f52bad6c0
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-02-22 10:54:05 +0900

    backEnd-setting

---
+++ src/main/java/Backend_TruckSnack/TruckSnack/TruckSnackApplication.java
@@ -0,0 +1,13 @@
+package Backend_TruckSnack.TruckSnack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckSnackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckSnackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Backend_TruckSnack/TruckSnack/TruckSnackApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend_TruckSnack.TruckSnack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckSnackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
